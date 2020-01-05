f94de578d25286565174bde4881b5027695d26e2
Dzhem <dzhem.aptula@gmail.com>
2020-01-05 03:33:03 +0100

    Edit variable names, remove bin, add synchronized display(?)

--- src/domein/Waiter.java
+++ src/domein/Waiter.java
@@ -4,12 +4,12 @@ import java.security.SecureRandom;
 
 public class Waiter implements Runnable{
     private static final SecureRandom generator = new SecureRandom();
-    private final String naam;
+    private final String name;
     Restaurant restaurant;
 
-    public Waiter(Restaurant restaurant, String naam){
+    public Waiter(Restaurant restaurant, String name){
         this.restaurant = restaurant;
-        this.naam = naam;
+        this.name = name;
     }
 
     @Override
@@ -18,12 +18,15 @@ public class Waiter implements Runnable{
 
                 try {
                     Order order = restaurant.takeOrder();
-                    System.out.printf("Waiter %s is bringing %s%n", naam, order);
+                    displayState(String.format("Waiter %s is bringing %s%n", name, order));
                     Thread.sleep(generator.nextInt(2000));
                 } catch (Exception e) {
                     e.printStackTrace();
                     Thread.currentThread().interrupt();
                 }
             }
+    }
+    private synchronized void displayState(String operation){
+        System.out.printf("%s", operation);
     }
 }
